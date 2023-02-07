b29569b87b0bdc2575b40d05334e25c2501ef95a
Tomas Makaj <Tmakajcoding@gmail.com>
2023-02-07 13:24:32 -0500

    commit tree f2f379ce5a01aa7be10d1ffc575d233e59b95916

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(a){
+    return a.slice(0,2)
+}
+const returnLastTwoDrivers = function(b){
+    return b.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function (fareMultiplier){
+    return function(value) {
+        return fareMultiplier * value
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers,cars) => {
+    return cars(drivers)
+}
